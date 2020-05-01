e563524cd8edae0f1d2167f1fdb86e07b2a8750e
Evandro <evandro.andrade@compasso.com.br>
2020-05-01 20:43:50 -0300

    Incluido o nodemon no ambiente desenvolvimento

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
-app.get('/atendimentos', (req, res) => res.send('Servidor rodando, tudo ok'))
+app.get('/atendimentos', (req, res) => res.send('Servidor rodando, tudo ok e esta realizando um GET'))
